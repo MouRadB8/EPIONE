72e3e3aab175804e82fdb379757daae70355c18b
MouRadB8 <moradkablaoui58@gmail.com>
2023-05-08 12:04:50 +0100

    modify

--- EPIONE-frontEnd/src/Components/Home/Home.jsx
+++ EPIONE-frontEnd/src/Components/Home/Home.jsx
@@ -1,10 +1,35 @@
 import React, { Fragment } from "react";
 import Header from "./Header";
-
+import './Home.css';
 const Home = ()=>{
     return(
         <Fragment>
             <Header/> 
+            <section className="numbers">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-3">
+                            <h2>1234+</h2>
+                            <h6>SAVINGS</h6>
+                        </div>
+                        <div className="col-md-3">
+                            <h2>8394+</h2>
+                            <h6>PHOTOS</h6>
+                        </div>
+                        <div className="col-md-3">
+                            <h2>1204+</h2>
+                            <h6>PRODUCTS</h6>
+                        </div>
+                        <div className="col-md-3">
+                            <h2>1634+</h2>
+                            <h6>GLOBES</h6>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </section>
         </Fragment>
         
 
